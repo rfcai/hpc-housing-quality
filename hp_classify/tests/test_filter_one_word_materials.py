b7db9c50ee0f77f80ef3c338c8195e3cf25e21dd
Joseph Frostad <jfrostad@uw.edu>
2018-12-11 12:59:22 -0800

    forgot to import pytest to the filter testing script

--- hp_classify/tests/test_filter_one_word_materials.py
+++ hp_classify/tests/test_filter_one_word_materials.py
@@ -5,6 +5,7 @@
 
 #import custom modules fpr testing
 import os
+import pytest
 import sys
 sys.path.append('.')
 import prep.prep_data as prep
@@ -45,13 +46,13 @@ df_clean = df_clean.dropna(subset=[DEP_VAR])
 def test_expected_number_of_rows():
     """Has the function successfully filtered out all the materials described with more than one word?"""
 
-    df = df_clean[0:20]
-    assert (sem.filter_one_word_materials(df, DEP_VAR).shape[0],
-            sum(df[DEP_VAR].str.get_dummies(sep=' ').T.sum() == 1))
+    test_df = df_clean[0:20]
+    assert (sem.filter_one_word_materials(test_df, DEP_VAR).shape[0] ==
+            sum(test_df[DEP_VAR].str.get_dummies(sep=' ').T.sum() == 1))
 
 def test_raise_error_if_no_material_with_one_word():
     """Does the function raise an error if there is no material described with one word in the corpus?"""
 
-    with pytest.raises(NoOneWordException):
-        df = df_clean[0:2]
-        sem.filter_one_word_materials(df, DEP_VAR)
+    with pytest.raises(sem.NoOneWordException):
+        test_df = df_clean[0:2]
+        sem.filter_one_word_materials(test_df, DEP_VAR)
